8f9423473bb0bf1c1523befc4bd79bbe706a7c1c
Lianyong Gao <liagao@microsoft.com>
2024-01-22 14:50:39 +0800

    commit

--- ThreadPoolPriorityBenchmark/Program.cs
+++ ThreadPoolPriorityBenchmark/Program.cs
@@ -1,5 +1,6 @@
 ﻿namespace ThreadPoolPriorityBenchmark
 {
+    using BenchmarkDotNet.Attributes;
     using BenchmarkDotNet.Running;
 
     public class Program
@@ -10,8 +11,54 @@
 
             //BenchmarkRunner.Run<ThreadPoolPriorityTest>();
 
-            BenchmarkRunner.Run<EnumToStringTest>();
+            BenchmarkRunner.Run<TestWithParamsBenchemark>();
+
             Console.ReadLine();
+        }
+    }
+
+
+    [MemoryDiagnoser]
+    public class TestWithParamsBenchemark
+    {
+        public TestWithParamsBenchemark()
+        {
+        }
+
+        [Params(10000)]
+        public int Loop;
+
+        public int[] array;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            this.array = new int[Loop];
+        }
+
+        [Benchmark]
+        public void TestWithToString()
+        {
+            var testVar = Array.Empty<byte>();
+            if(testVar.Length != 0)
+            {
+                throw new Exception("!!!");
+            }
+        }
+
+        [Benchmark]
+        public void TestWithNameOf()
+        {
+            var testVar = new byte[0];
+            if (testVar.Length != 0)
+            {
+                throw new Exception("!!!");
+            }
+        }
+
+        private int HandleHashCode(string test, int i)
+        {
+            return test.GetHashCode() + i;
         }
     }
 }
